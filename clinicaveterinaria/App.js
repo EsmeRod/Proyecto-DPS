71f4c0a96398d78b0cdd71b65aba6c0f62064617
EAGPsting <93798048+EAGPsting@users.noreply.github.com>
2022-10-12 17:14:26 -0700

    Avance 2
    Avance 2

--- clinicaveterinaria/App.js
+++ clinicaveterinaria/App.js
@@ -1,38 +1,22 @@
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet,TouchableOpacity } from 'react-native';
 import Constants from 'expo-constants';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import MenuPrincipal from './Components/MenuPrincipal'
+import Login from './Components/Loggin'
+import Navigation from './Navigation';
 
 // You can import from local files
-import AssetExample from './components/AssetExample';
-
-// or any pure javascript modules available in npm
-import { Card } from 'react-native-paper';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.paragraph}>
-        Change code in the editor and watch it change on your phone! Save to get a shareable url.
-      </Text>
-      <Card>
-        <AssetExample />
-      </Card>
-    </View>
+
+ 
+    <Navigation />
+
+   
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
-    padding: 8,
-  },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-});
+

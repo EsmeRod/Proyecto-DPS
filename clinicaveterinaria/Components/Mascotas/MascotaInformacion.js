1b01a009d913614bd86c5ffedd46f17c03bfc495
IsmaelMarin <ismaelmarin380@gmail.com>
2022-11-08 10:20:33 -0600

    Splah and Login

---
+++ clinicaveterinaria/Components/Mascotas/MascotaInformacion.js
@@ -0,0 +1,113 @@
+import React, {useState} from 'react';
+import { Text, View, StyleSheet, Image, ScrollView } from 'react-native';
+import Constants from 'expo-constants';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { Ionicons } from '@expo/vector-icons';
+import { Fontisto } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons'; 
+// or any pure javascript modules available in npm
+import { Card, ProgressBar } from 'react-native-paper';
+
+export default function Mascota() {
+  const [Name, setName] = useState("Pamela");
+  const [Raza, setRaza] = useState("Bichon Maltes");
+  const [Nacimiento, setNacimiento] = useState("05/03/2002");
+  const [Edad, setEdad] = useState("2Y 4M 25D");
+  const [Progreso, setProgreso] = useState(0.4);
+  const [Colorprog, setColorprog] = useState('green');
+  if(Progreso>=0.3){
+    const [Colorprog, setColorprog] = useState('green');
+  }else{
+    const [Colorprog, setColorprog] = useState('red');
+  }
+  return (
+    <>
+    <ScrollView>
+    <View style={styles.container}>
+      
+      <Text>Acá tiene que ir la imagen de la mascota</Text>
+
+
+      <Text style={styles.box}>
+      <Ionicons name= "female" />
+      <Text style={styles.paragraph}>{Name}{"\n"}
+        {"\n"}</Text>
+      <Text>{Raza}{"\n"}
+        {"\n"}</Text>
+        <Ionicons name= "gift" />
+      <Text>{Nacimiento}{"\n"}
+        {"\n"} </Text>
+      <Ionicons name= "calendar" />
+        <Text>{Edad}</Text>
+      </Text>
+      </View>
+      <View style={styles.containerimg}>
+      <Text style={styles.box,styles.icontxt}>
+      <Ionicons name= "water" style={styles.iconS} size={70}/>
+        Baño
+      </Text>
+            <ProgressBar progress={Progreso} color={Colorprog}/>
+      <Text style={styles.box,styles.icontxt}>
+        <Ionicons name= "cut" style={styles.iconS} size={70}/>
+          Pelo
+      </Text>
+            <ProgressBar progress={Progreso} color={Colorprog}/>
+      <Text style={styles.box,styles.icontxt}>
+      <Fontisto name= "injection-syringe" style={styles.iconS} size={70}/>
+        Vacuna
+      </Text>
+            <ProgressBar progress={Progreso} color={Colorprog}/>
+      <Text style={styles.box,styles.icontxt}>
+      <FontAwesome5 name= "tooth" style={styles.iconS} size={70}/>
+        Dientes
+      </Text>
+            <ProgressBar progress={Progreso} color={Colorprog}/>
+      </View>
+      </ScrollView>
+      </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 5,
+  },
+  containerimg:{
+    backgroundColor:'white',
+  },
+  img: {
+    flex:1,
+    alignSelf: 'flex-start',
+    alignItems:'middle',
+    margin: 5,
+    height: 130,
+    width: '100%',
+  },
+  box:{
+     flex: 1,
+     margin: 10
+  },
+  iconS:{
+    size: 50,
+    margin:40,
+    alignSelf: 'space-around',
+  },
+  icontxt:{
+    margin: 15,
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'middle-center',
+  },
+  paragraph: {
+    flex:1,
+    margin: 8,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'left',
+  },
+});
---
+++ clinicaveterinaria/Components/Navigation.js
@@ -0,0 +1,194 @@
+import React from "react";
+import { Text, View, StyleSheet,TouchableOpacity,SafeAreaView } from 'react-native';
+import { createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import {NavigationContainer} from "@react-navigation/native";
+import{createNativeStackNavigator} from "@react-navigation/native-stack";
+import Home from 'react-native-vector-icons/FontAwesome5';
+import Carrito from 'react-native-vector-icons/MaterialIcons';
+import Calendario from 'react-native-vector-icons/FontAwesome5';
+import Comprar from 'react-native-vector-icons/FontAwesome5';
+import Huella from 'react-native-vector-icons/MaterialIcons';
+import Login from './Perfil/Login';
+//Screens
+
+ import CarritoPrincipal from "../Components/Carrito/CarritoPrincipal";
+  import InicioMenuPrincipal from "../Components/Inicio/MenuPrincipal";
+ import CitasPrincipal from "../Components/Citas/CitasPrincipal";
+ import CitaCrear from "../Components/Citas/CitaCrear";
+ import StackScreen from "../Components/Total/StackScreen";
+ import MascotaPrincipal from "../Components/Mascotas/MascotaPrincipal";
+ import Perfil from "../Components/Mascotas/MascotaPerfil";
+ import MascotaInfo from "../Components/Mascotas/MascotaInformacion";
+ //Agrega el componete Perfil en el Stack
+
+ const MascotaStack = createNativeStackNavigator();
+ const FunctionMascotaStack = () => {
+
+   return(
+<MascotaStack.Navigator initialRouteName="Mascota"> 
+        <MascotaStack.Screen
+          name="Mascota"
+          component={MascotaPrincipal}
+          options={{headerBackTitleVisible: false,
+          headerTitleAlign:'center',
+          headerStyle:{backgroundColor:"#29EAAA"}}}
+        />
+        <MascotaStack.Screen name="Perfil" component={Perfil}
+        options={{headerBackTitleVisible: false,
+        headerTitleAlign:'center',
+        headerStyle:{backgroundColor:"#FFD959"}}} />
+
+        <MascotaStack.Screen name="MascotaInfo" component={MascotaInfo}
+        options={{headerBackTitleVisible: false,
+        headerTitleAlign:'center',
+        headerStyle:{backgroundColor:"#FFD959"}}} />
+      </MascotaStack.Navigator>
+   );
+ };
+
+
+
+const CitasStack = createNativeStackNavigator();
+const FunctionCitasStack = () => {
+  return (
+    
+      <CitasStack.Navigator initialRouteName="Citas"> 
+        <CitasStack.Screen
+          name="Citas"
+          component={CitasPrincipal}
+          options={{headerBackTitleVisible: false,
+          headerTitleAlign:'center',
+          headerStyle:{backgroundColor:"#29EAAA"}}}
+        />
+        <CitasStack.Screen name="CitaCrear" component={CitaCrear}
+        options={{headerBackTitleVisible: false,
+        headerTitleAlign:'center',
+        headerStyle:{backgroundColor:"#FFD959"}}} />
+      </CitasStack.Navigator>
+
+      
+    
+  );
+};
+
+ const InicioStack = createNativeStackNavigator();
+ function FunctionInicioStack() {
+   return(
+     <InicioStack.Navigator
+     initialRouteName="Inicio">
+     <InicioStack.Screen name="Inicio"
+     component={InicioMenuPrincipal}
+     options={{headerBackTitleVisible: false,
+      headerTitleAlign:'center',
+      headerStyle:{backgroundColor:"#29E4EA"}}}/>
+
+     <InicioStack.Screen name="Mascota"
+     component={MascotaPrincipal}
+      options={{headerBackTitleVisible: false,
+      headerTitleAlign:'center',
+      headerStyle:{backgroundColor:"#29EAAA"}}}/>
+
+     <InicioStack.Screen name="Citas"
+     component={CitasPrincipal}
+     options={{headerBackTitleVisible: false,
+      headerTitleAlign:'center',
+      headerStyle:{backgroundColor:"#29EAAA"}}}/>
+
+      <InicioStack.Screen name="CitaCrear" component={CitaCrear}
+        options={{headerBackTitleVisible: false,
+        headerTitleAlign:'center',
+        headerStyle:{backgroundColor:"#FFD959"}}} />
+
+      <InicioStack.Screen name="Carrito"
+     component={CarritoPrincipal}
+     options={{headerBackTitleVisible: false,
+      headerTitleAlign:'center',
+      headerStyle:{backgroundColor:"#29EAAA"}}}/>
+
+      <InicioStack.Screen name="MascotaInfo" component={MascotaInfo}
+        options={{headerBackTitleVisible: false,
+        headerTitleAlign:'center',
+        headerStyle:{backgroundColor:"#FFD959"}}} />
+
+      <InicioStack.Screen name="Perfil" component={Perfil}
+        options={{headerBackTitleVisible: false,
+        headerTitleAlign:'center',
+        headerStyle:{backgroundColor:"#FFD959"}}} />
+
+     </InicioStack.Navigator>
+
+   )
+ }
+
+ const Tab = createBottomTabNavigator();
+
+ function MyTabs() {
+
+    return(
+      <Tab.Navigator
+      initialRouteName="InicioMenuPrincipal" 
+      screenOptions={{
+        tabBarInactiveBackgroundColor:'white',
+        tabBarInactiveTintColor:'#3D975F'
+      }}
+      >
+        <Tab.Screen name="Pago" component={StackScreen}
+       options={{
+         tabBarLabel:'Total',
+         tabBarIcon:({color,size}) => (
+       <Comprar name="credit-card" color={color} size={size}></Comprar>
+         ),
+         headerShown:false,
+         
+       }} />
+       <Tab.Screen name="MascotaPrincipal" component={FunctionMascotaStack}
+       options={{
+         tabBarLabel:'Mascotas',
+         tabBarIcon:({color,size}) => (
+       <Huella name="pets" color={color} size={size}></Huella>
+         ),
+         headerShown:false,
+       }} />
+
+       <Tab.Screen name="InicioMenuPrincipal" component={FunctionInicioStack}
+       options={{
+         tabBarLabel:'Inicio',
+         tabBarIcon:({color,size}) => (
+       <Home name="home" color={color} size={size}></Home>
+         ),
+         headerShown:false,
+         
+       }} />
+
+       <Tab.Screen name="CitasPrincipal" component={FunctionCitasStack} 
+       options={{
+         tabBarLabel:'Citas',
+         tabBarIcon:({color,size}) => (
+       <Calendario name="calendar-plus" color={color} size={size}></Calendario>
+         ),
+         headerShown:false,
+       }}/>
+       
+        <Tab.Screen name="CarritoPrincipal" component={CarritoPrincipal} options={{
+         tabBarLabel:'Carrito',
+         tabBarIcon:({color,size}) => (
+       <Carrito name="add-shopping-cart" color={color} size={size}></Carrito>
+         ),
+         headerShown:false,
+       }} />
+
+      </Tab.Navigator>
+
+    );
+ }
+ export default function Navigation(){
+    return(
+
+       <MyTabs />
+       
+      
+     
+    );
+ }
+
+
